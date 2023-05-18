4311a5383b6137292faa712c6f0a030a909d66ff
“Vikas <vpawar01@entrata.com>
2023-05-18 17:15:13 +0530

    code ceanup

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ const app = express();
 app.use(express.json());
 
 app.get('/api/todos', async (req, res) => {
-	const arr = ['d', 9.98, [8, 'r', [-5, 9.7, 'a']]];
-
-	const result = [];
-	const flatArray = (inputArr) => {
-		for (let i = 0; i < inputArr.length; i++) {
-			if (inputArr[i] > 0) {
-				flatArray(inputArr[i]);
-			} else {
-				result.push(inputArr[i]);
-			}
-		}
-	};
-
 	res.json([{ id: 1, name: 'Todo 1' }]);
 });
 
